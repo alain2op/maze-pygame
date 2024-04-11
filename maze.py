2c4721801e42c529d483879acc9e9064e7cd2f64
alain2op <cbsujanreddy@gmail.com>
2024-04-11 15:22:42 +0530

    the one with loops

--- maze.py
+++ maze.py
@@ -2,14 +2,13 @@ import numpy as np
 import pygame
 import random
 import math
-def tile_checker(maze):
+def tile_checker(maze,tile):
     rows, cols = maze.shape
-
+    check=True
     for i in range(rows - 1):
         for j in range(cols - 1):
             if maze[i, j] == 0 and maze[i, j+1] == 0 and maze[i+1, j] == 0 and maze[i+1, j+1] == 0:
                 return False
-    
     return True
 #after each iteration, the probability to move towards end point increases of the path using this fn
 def probs_change(probs,inc,size):
@@ -17,11 +16,11 @@ def probs_change(probs,inc,size):
         if i in inc:
             probs[i]+=0.02*8/(size+1)
             if probs[i]>0.5:
-                probs[i]=0.5
+                probs[i]=0.4
         else:
             probs[i]-=0.02*8/(size+1)
             if probs[i]<0:
-                probs[i]=0
+                probs[i]=0.1
     return probs
 #if the path reaches edge, it can only have 3 choices
 def edge_choice(probs, move, moves):
@@ -73,7 +72,7 @@ def path_generator(size,start,end):
                 tile[0]+=choice[0]
                 tile[1]+=choice[1]
                 maze[int(tile[0]),int(tile[1])]=0
-                if(tile_checker(maze)):
+                if(tile_checker(maze,tile)):
                     probs=probs_change(probs,inc,size)
                     break
                 else:
@@ -96,7 +95,8 @@ def maze_generator(level):
     else:
         end=np.array([0,size-1])
     maze=path_generator(size,start,end)
-    for i in range(0):
+    path=np.copy(maze)
+    for i in range(random.randint(150,300)):
         zeros=np.where(maze==0)
         poss_moves=[]
         rand_zero=random.randint(0,zeros[0].size-1)
@@ -117,22 +117,25 @@ def maze_generator(level):
             move=random.choice(poss_moves)
             tile+=moves[move]
             maze[int(tile[0]),int(tile[1])]=0
-            if tile_checker(maze):
+            if tile_checker(maze,tile):
                 break
             else:
                 maze[int(tile[0]),int(tile[1])]=1
                 tile-=moves[move]
                 poss_moves.remove(move)
-    return maze
+    return maze,path
 
 pygame.init()
 tile_size = 50
-maze = maze_generator(5)
+maze,path = maze_generator(5)
 window_width = maze.shape[1] * tile_size
 window_height = maze.shape[0] * tile_size
 window = pygame.display.set_mode((window_width, window_height))
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE=(0,0,255)
+RED=(255,0,0)
+GREEN=(0,255,0)
 running = True
 while running:
     for event in pygame.event.get():
@@ -143,8 +146,10 @@ while running:
         for x in range(maze.shape[1]):
             if maze[y, x] == 1:
                 color = BLACK
+            elif path[y,x]==0:
+                color = GREEN
             else:
-                color = WHITE
+                color=WHITE
             pygame.draw.rect(window, color, (x * tile_size, y * tile_size, tile_size, tile_size))
     pygame.display.update()
 pygame.quit()
